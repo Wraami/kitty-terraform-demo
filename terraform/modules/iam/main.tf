f0d17f102d4485453321c931eedc5bcceb59fbf7
Wraami <83980439+Wraami@users.noreply.github.com>
2026-03-26 08:35:04 +0000

    chore: add more hints to IAM

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,25 +1,25 @@
 # # TASK 4: IAM POLICY FUN!
 # resource "aws_iam_role" "treat-di" {
-#   name = "$treat-dispenser-role"
+#   name = "$treat-dispenser-role" # boss would love a variable here, but an answer is in check_progress...
 
 #   assume_role_policy = jsonencode({
 #     Version = ""
 #     Statement = [{
 #       Action = "sts:AssumeRole"
 #       Effect = "Allow"
-#       Principal = "AWS = "arn:aws:iam::000000000000:" # hmm...
+#       Principal = "AWS = "arn:aws:iam::000000000000:" # hmm the account looks right, but somethings missing...
 #     }]
 #   })
 # }
 
 # resource "aws_iam_role_policy" "treat_access" {
 #   name = "${var.env_naem{TreatAccessPolicy"
-#   role = aws_iam_role..
+#   role = aws_iam_role.. # whats the resource name above this?
 
 #   policy = jsonencode({
-#     Version = "2-17"
+#     Version = "2-17" # What year did these versions start...
 #     Statement = [{
-#       Action   = ["s3:GetObjects", "s3:ListBuckets"]
+#       Action   = ["s3:GetObjects", "s3:ListBuckets"] # Consult your actions site!
 #       Effect   = "Deny"
 #       Resource = ["arn:aws:s3:::bosses-imaginary-bucket/*"] #what could this value be..?
 #     }]
