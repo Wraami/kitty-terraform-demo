4c233ad067cf342f706fd0ab52abb1b6772003c4
Wraami <83980439+Wraami@users.noreply.github.com>
2026-03-26 01:14:34 +0000

    chore: add powerpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,16 +3,16 @@ module "s3" {
   # pass variables as needed
 }
 
-module "dynamo" {
-  source = "../../modules/dynamo"
-  # pass variables as needed
-}
+# module "dynamo" {
+#   source = "../../modules/dynamo"
+#   # pass variables as needed
+# }
 
-module "iam" {
-  source = "../modules/iam"
-}
+# module "iam" 
+#   source = "../modules/iam"
+# }
 
-module "messaging" {
-  source = "../../modules/messaging"
-  # pass variables as needed
-}
+# module "messaging" {
+#   source = "../../modules/messaging"
+#   # pass variables as needed
+# }
